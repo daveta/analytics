c94b3b890840bad3fa3b018de6503092e188aec4
Dave Taniguchi <daveta@microsoft.com>
2018-12-26 16:11:46 -0800

    Add comments

--- makeAppInsightsUrl/ExportQueryHelper.cs
+++ makeAppInsightsUrl/ExportQueryHelper.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Bot.Configuration;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Bot.Configuration;
 using System;
 using System.Globalization;
 using System.IO;
